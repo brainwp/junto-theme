ffbf87c13d0b2a89c837a3c8c05a576f37f01845
everaldomatias <matiaseveraldo@gmail.com>
2014-07-02 16:59:54 -0300

    Define .container

--- footer.php
+++ footer.php
@@ -8,16 +8,17 @@
  */
 ?>
 
-	</div><!-- #content -->
+		</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'junto-theme' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'junto-theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'junto-theme' ), 'Junto Theme', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'junto-theme' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'junto-theme' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'junto-theme' ), 'Junto Theme', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+</div><!-- container -->
 
 <?php wp_footer(); ?>
 
