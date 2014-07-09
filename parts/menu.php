11d45ad339cb5b7b893ea27ddfd62e293f998c39
everaldomatias <matiaseveraldo@gmail.com>
2014-07-09 12:26:34 -0300

    Pegando o ID da pagina Clientes para exibir item no menu.

--- parts/menu.php
+++ parts/menu.php
@@ -8,6 +8,8 @@
 
 <a class="top" href="#top">Junto</a>
 
-<?php if ( get_field('op_adicionar_clientes_menu', 'option') ) : ?>
+<?php
+$c = get_page_by_title('clientes', 'OBJECT', 'onepage');
+if ( get_field('op_exibir_menu_clientes', $c->ID) ) : ?>
 	<a class="menu-clientes" href="#clientes">Clientes</a>
 <?php endif; ?>
