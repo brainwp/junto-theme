468d65896e3f39d9d1c521673ed43996da76de09
everaldomatias <everaldo@brasa.art.br>
2014-08-26 16:20:15 -0300

    Aplicando CSS ao archive-portcolio.php

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -9,23 +9,21 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<section id="primary" <?php post_class('col-md-12 content-area'); ?>>
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title">
-					Cases / Portfólio
-				</h1>
+				<h1 class="entry-title">Cases</h1>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
+			<?php $count = ''; ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
                 <li class="col-md-3 each-case">
                     <a href="<?php the_permalink(); ?>">
-
 						<div class="img">
                         <?php
 	                        if (has_post_thumbnail()) {
@@ -35,14 +33,19 @@ get_header(); ?>
 	                            echo '<img src="' . get_bloginfo('template_url') . '/img/thumb-portfolio.png"/>';
 	                        }
                         ?>
-                        </div>
+                        </div><!-- .img -->
                         <div class="col-md-12 desc">
                             <?php the_title(); ?>
                         </div><!-- .desc -->
                     </a>
-
                 </li><!-- .col-md-3 each-case -->
 
+				<?php $count++; ?>
+
+                <?php if ($count == 4) : ?>
+                	<div class="clear"></div>
+                	<?php $count = ''; ?>
+                <?php endif; ?>
 
 			<?php endwhile; ?>
 
@@ -57,5 +60,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
